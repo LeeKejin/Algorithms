a4e8ba277431172b1ddfe3dcea4360ab8a91f1af
LeeKejin <lkj227yzuedu@gmail.com>
2021-10-24 15:28:23 +1100

    Rebase 3

--- src/com/educative/graph/Test.java
+++ src/com/educative/graph/Test.java
@@ -19,12 +19,11 @@ public class Test {
         g2.printGraph();
 
         System.out.println(bfs(g2));
-        System.out.println("123");
-
+        System.out.println("12345");
         String str = "Test";
         System.out.println(str);
-        System.out.println("12345");
-
+        System.out.println("426888");
+        System.out.println("9999999");
         String s = "s1";
         System.out.println(s);
 
